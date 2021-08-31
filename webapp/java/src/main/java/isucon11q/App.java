be4b469f5950a28a271bb8b8bf246f640d79df4d
ockie1729 <you_oki1@yahoo.co.jp>
2021-09-01 01:45:18 +0900

    fix(java) マッピングを追加

--- webapp/java/src/main/java/isucon11q/App.java
+++ webapp/java/src/main/java/isucon11q/App.java
@@ -1,10 +1,16 @@
 package isucon11q;
 
+import isucon11q.model.Isu;
+import isucon11q.repository.IsuRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -14,8 +20,12 @@ public class App {
 		SpringApplication.run(App.class, args);
 	}
 
+	@Autowired
+	IsuRepository isuRepository;
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	String hello() {
-		return "hello! from isucon11q";
+		List<Isu> isuList = isuRepository.findById(1);
+		return "hello! from isucon11q: first isu uuid:" + isuList.get(0).getJiaIsuUUID();
 	}
 }
--- webapp/java/src/main/java/isucon11q/model/Isu.java
+++ webapp/java/src/main/java/isucon11q/model/Isu.java
@@ -13,6 +13,62 @@ public class Isu implements Serializable {
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
+    public String getJiaIsuUUID() {
+        return jiaIsuUUID;
+    }
+
+    public void setJiaIsuUUID(String jiaIsuUUID) {
+        this.jiaIsuUUID = jiaIsuUUID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+//    public byte[] getImage() {
+//        return image;
+//    }
+//
+//    public void setImage(byte[] image) {
+//        this.image = image;
+//    }
+
+    public String getCharacter() {
+        return character;
+    }
+
+    public void setCharacter(String character) {
+        this.character = character;
+    }
+
+    public String getJiaUserID() {
+        return jiaUserID;
+    }
+
+    public void setJiaUserID(String jiaUserID) {
+        this.jiaUserID = jiaUserID;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
     public Integer getId() {
         return id;
     }
